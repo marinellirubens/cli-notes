396358d114782d6df0bbf4b968d5fd0f4a5908e6
marinellirubens <marinelli.rubens@gmail.com>
2024-01-31 21:32:37 -0300

    Included autocompletion example

--- Makefile
+++ Makefile
@@ -3,5 +3,6 @@ run:
 compile:
 	go build -o build/notes golang_notes.go
 install:
+	cp -f ./completions/fish/notes.fish /usr/share/fish/completions/notes.fish
 	go install
 
---
+++ completions/fish/notes.fish
@@ -0,0 +1,5 @@
+# Global flags
+complete -c notes -s h -l help -d "Show context-sensitive help."
+complete -c notes -s A -l color-always -d "Enable color output always"
+complete -c notes -l no-color -d "Disable color output"
+complete -c notes -l version -d "Show application version."
