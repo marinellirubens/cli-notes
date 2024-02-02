58b8941f15d6c0e25044fccc3e88226edd3193bb
marinellirubens <marinelli.rubens@gmail.com>
2024-02-01 21:57:08 -0300

    Updated the shell completions for fish and zsh

--- completions/fish/notes.fish
+++ completions/fish/notes.fish
@@ -1,8 +1,6 @@
 # Global flags
 complete -c notes -s h -l help -d "Show context-sensitive help."
-complete -c notes -s A -l color-always -d "Enable color output always"
-complete -c notes -l no-color -d "Disable color output"
-complete -c notes -l version -d "Show application version."
+complete -c notes -s v -l version -d "Show application version."
 
 # Subcommands
 complete -c notes -n '__fish_use_subcommand' -xa 'help' -d "Show help."
@@ -12,3 +10,7 @@ complete -c notes -n '__fish_use_subcommand' -xa 'delete' -d "delete a note by f
 complete -c notes -n '__fish_use_subcommand' -xa 'get' -d "Print the content of a note by file name"
 complete -c notes -n '__fish_use_subcommand' -xa 'export' -d "Export notes to a zip file"
 complete -c notes -n '__fish_use_subcommand' -xa 'import' -d "Import notes from a zip file"
+
+complete -c notes -n '__fish_seen_subcommand_from export' -s o -l output -d "Output file name, (default: .notes.zip)"
+complete -c notes -n '__fish_seen_subcommand_from import' -s i -l input -d "Input file name, (default: .notes.zip)"
+
